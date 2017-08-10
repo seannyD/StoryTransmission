45788f5d1fffa2b842d48cc9e2fc66c0aedf43c6
seannyD <sean.g.roberts@gmail.com>
2017-08-10 10:48:37 +0100

    improved data summaries

--- stats/getData.R
+++ stats/getData.R
@@ -1,13 +1,75 @@
+library(entropy)
+
+# There should be the following file structure:
+# Transmission (top folder)
+#     stats
+#         (save this R file here)
+#     results
+#           (where results will be saved)
+#           UK
+#           USA
+#     OnlineBackups
+#           7Aug (or backup date)
+#                 distractionTask
+#                 logs
+#                 qualifyingSurvey
+#                 recordings
+#                 survey
+
+# Set this folder to the 'stats' folder
+#
 setwd("~/Documents/Bristol/Transmission/stats/")
 
-# This assumes that there's a folder somewhere with the sub-fodlers:
-# distractionTask, logs, qualifyingSurvey, recordings, survey
-
+# Set 'backupfolder' to the location of the folder you downloaded things to.
 # should include final backslash
 backupfolder = "../OnlineBackups/7Aug/"
 
+
+
+########
+
 fileList = list.files(paste(backupfolder,"logs/",sep=''),"Files*")
 
+
+
+
+# Functions
+
+timeDifference = function(t1,t2){
+  sapply(1:length(t1), function(i){
+    x = capture.output(strptime(t2[i], format="%Y-%m-%d %H:%M:%S")- strptime(t1[i], format="%Y-%m-%d %H:%M:%S"))
+    x = gsub("Time difference of ","",x)
+  })
+}
+
+recordingFileSize = function(fs){
+  fs = strsplit(fs,",")[[1]]
+  s = sum(file.size(paste0(backupfolder,"recordings/",justFilename(fs))))
+  return(utils:::format.object_size(s, "auto"))
+}
+
+prioritiseOrder = function(dx,o){
+  return(dx[,c(
+    o,
+    names(dx)[!names(dx) %in% o]
+  )])
+}
+
+getSpEvalEntropy = function(dx){
+  sp = names(dx)[grepl("SpEval_",names(dx))]
+  ex = c("QuestionOrder","_MT_","evaluationPrestige","evaluationPresentationNumber","localisation","startTime","endTime","Recording1File","Recording2File","Story2","_Story1","ParticipantId","Location",'evaluationFile')
+  for(x in ex){
+    sp = sp[!grepl(x,sp)]
+  }
+  
+  sapply(1:nrow(dx), function(i){
+  freqs = table(as.vector(unlist(dx[i,sp])))
+  ps = freqs/sum(freqs)
+  # H = -sum(ifelse(ps > 0, ps * log(ps), 0))
+  # table will never return zero count
+  return(-sum( ps * log(ps)) / log(sum(freqs)))
+  })
+}
 
 justFilename = function(X){
   sapply(X, function(x){
@@ -88,6 +150,36 @@ getDistractionTaskData = function(distractionTasks, participantId, fl){
 }
 
 
+findTimeFile = function(pid){
+  # Find the amount of time the participant did not have the window focussed
+  print("FTF")
+  print(pid)
+  tfL = list.files(paste0(backupfolder,"logs/"),"Time*")
+  for(f in tfL){
+    dx = read.csv(paste0(backupfolder,"logs/",f), stringsAsFactors = F)
+    if(dx$participantId[1]==pid){
+      numberBlurTimes = sum(grepl("blur window",dx$Message))
+      totalTD = 0
+      if(numberBlurTimes>0){
+        totalTD = sum(sapply(which(grepl("blur window", dx$Message)), function(i){
+          t1 = dx[i,]$Time
+          if(nrow(dx)>=(i+1)){
+            t2 = dx[i+1,]$Time
+            return(as.numeric(difftime(strptime(t2, format="%Y-%m-%d %H:%M:%S"), strptime(t1, format="%Y-%m-%d %H:%M:%S"), units = 'secs')))
+          } else{
+            return(NA)
+          }
+        }), na.rm=T)
+      }
+      return(c(f,numberBlurTimes,totalTD))
+    }
+  }
+  return(c(NA,NA,NA))
+}
+
+
+#########
+
 processFileLog = function(filename){
   print(filename)
   fl = read.csv(
@@ -99,6 +191,9 @@ processFileLog = function(filename){
   if(nrow(fl)>2){
     highPFiles = paste(fl[grepl("highp",tolower(fl$filetype)),]$filename, collapse = ",")
     lowPFiles = paste(fl[grepl("lowp",tolower(fl$filetype)),]$filename, collapse = ",")
+    
+    highPFiles.size = recordingFileSize(highPFiles)
+    lowPFiles.size = recordingFileSize(lowPFiles)
     
     storyOrder = "HighP,LowP"
     # TODO: check if uppercase is correct?
@@ -147,16 +242,29 @@ processFileLog = function(filename){
     # remove initial columns
     speechEvaluationResults = speechEvaluationResults[,2:ncol(speechEvaluationResults)]
     
+    # timeFiles
+    timeFiles = findTimeFile(fl$participantID[1])
+    
+    
+    
+    # Combine data
     pData = data.frame(
       participantID = fl$participantID[1],
       location = location,
+      highPRecording.size = highPFiles.size,
+      lowPRecording.size = lowPFiles.size,
+      numSwitchWindows = timeFiles[2],
+      secondsUnfocussedWindow = timeFiles[3],
       highPStory = highPStory,
       highPRecordings = highPFiles,
       lowPStory = lowPStory,
       lowPRecordings = lowPFiles,
       storyOrder = storyOrder,
       surveyFilename = surveyFilename,
-      speechEvaluationFiles = paste(speechEvaluationFiles, collapse=',')
+      speechEvaluationFiles = paste(speechEvaluationFiles, collapse=','),
+      logFile = filename,
+      timeFile = timeFiles[1]
+      
     )
     pData = cbind(pData,
                   distractionTaskResults,
@@ -192,9 +300,11 @@ createNiceResultsFolders = function(dataset, folder){
   
   for(i in 1:nrow(dataset)){
     
+    dt = strftime(dataset[i,]$startTime)
+    
     # make folder
     partFolder = paste(folder,
-                       strsplit(as.character(dataset[i,]$startTime)," ")[[1]][1],
+                       strsplit(dt," ")[[1]][1],
                        "_",
                        as.character(dataset$participantID[i]),
                        sep='')
@@ -243,6 +353,8 @@ createNiceResultsFolders = function(dataset, folder){
     evaluationFilesFrom = paste0(backupfolder,'survey/',evaluationFiles)
     evaluationFilesTo = rep(paste0(partFolder,"/Eval_",evaluationFiles))
     file.copy(evaluationFilesFrom,evaluationFilesTo)
+    
+    
   }
   
 }
@@ -267,8 +379,31 @@ for(f in fileList){
   }
 }
 
+results.UK = results.UK[order(results.UK$startTime),]
+results.USA = results.USA[order(results.USA$startTime),]
+
+
+results.UK$totalTime = timeDifference(results.UK$startTime, results.UK$endTime)
+results.USA$totalTime = timeDifference(results.USA$startTime, results.USA$endTime)
+
+results.USA$spEvalEnt = round(getSpEvalEntropy(results.USA),2)
+results.UK$spEvalEnt = round(getSpEvalEntropy(results.UK),2)
+
+colOrder = c("participantID",'location','highPRecording.size','lowPRecording.size','totalTime','spEvalEnt','Dist_Round_0_VSLT.p_plus_pnd','Dist_Round_1_VSLT.p_plus_pnd','numSwitchWindows','secondsUnfocussedWindow')
+
+results.USA = prioritiseOrder(results.USA,colOrder)
+results.UK = prioritiseOrder(results.UK,colOrder)
+
+
+
 write.csv(results.UK, file = '../results/Results_UK.csv')
 write.csv(results.USA, file = '../results/Results_USA.csv')
+
+write.csv(results.UK[,colOrder], file = '../results/Results_UK_short.csv')
+write.csv(results.USA[,colOrder], file = '../results/Results_USA_short.csv')
+
+### 
+# Copy files to nice folders
 
 results.UK = read.csv("../results/Results_UK.csv", stringsAsFactors = F)
 results.USA = read.csv("../results/Results_USA.csv", stringsAsFactors = F)

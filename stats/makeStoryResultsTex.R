df1366ee495b463adc328ba0e5ef1f701346e7e7
seannyD <sean.g.roberts@gmail.com>
2019-05-14 15:27:45 +0100

    add tex to pdf versions

--- stats/makeStoryResultsTex.R
+++ stats/makeStoryResultsTex.R
@@ -52,81 +52,173 @@ dp3 = dp3[dp3$participantID %in% parts,]
 
 #########
 # Latex
-makeLatex = function(dx,dxp3){
-  lTable = makeTable(dx,dxp3)
+makeLatex = function(dx,dxp2,dxp3){
+  lTable = makeLatexTable(dx,dxp2,dxp3)
+  
+  p1Important = paste(
+    paste("\\textbf{Phase1 most important Scene:} ",
+          dx$mostImportantSceneNumber)," (",dx$mostImportantSceneSRC,")",
+    "\\\\ \\textbf{Reason}:",
+    dx$mostImportantSceneReason,
+    "\\\\",collapse="\n")
+  p2Important  = ""
+  p3Important = ""
+  
+  if(nrow(dxp2)>0){
+    p2Important = paste(
+      paste("\\textbf{Phase2 most important Scene:} ",
+            dxp2$mostImportantSceneNumber)," (",dxp2$mostImportantSceneSRC,")",
+      "\\\\ \\textbf{Reason}:",
+      dxp2$mostImportantSceneReason,
+      "\\\\",collapse="\n")
+  }
+  
+  if(nrow(dxp3)>0){
+    p3Important = paste(
+      paste("\\textbf{Phase3 most important Scene:} ",
+            dxp3$mostImportantSceneNumber)," (",dxp3$mostImportantSceneSRC,")",
+      "\\\\ \\textbf{Reason}:",
+      dxp3$mostImportantSceneReason,
+      "\\\\",collapse="\n")
+  }
+  
+  
   paste(
     paste("\\section{",dx$participantID,"}"),
     dx$startTime,
     "\\\\",
-    paste("\\textbf{Phase1 most important Scene:} ",dx$mostImportantSceneNumber),
-    "\\\\ \\textbf{Reason}:",
-    dx$mostImportantSceneReason,
+    p1Important,
     "\\\\",
-    paste("\\textbf{Phase3 most important Scene:} ",dxp3$mostImportantSceneNumber),
-    "\\\\ \\textbf{Reason}:",
-    dxp3$mostImportantSceneReason,
+    p2Important,
     "\\\\",
+    p3Important,
+    "\\\\ \\newpage \\clearpage",
     lTable,
     "\\clearpage \\newpage",
     collapse="\n")
 }
 
-makeTable = function(dx,dxp3){
-  ix = paste0("image",1:16)
-  ix = ix[ix %in% names(dx)]
-  imageFiles= dx[,ix]
-  ifx = unlist(imageFiles)
-  ifx = ifx[ifx %in% names(dx)]
-  descriptions = dx[,ifx]
-  imageFilesX = paste0("\\includegraphics[width=0.2\\textwidth]{img/",imageFiles,"}")
+changeToIconPNG = function(X){
+  gsub("\\.jpg","icon.png",X)
+}
+
+makeLatexTable = function(dx,dxp2,dxp3){
+  imageFiles= dx[paste0("image",1:16)]
+  descriptions = dx[unlist(imageFiles)]
+  imageFilesX = paste0("\\includegraphics[width=0.09\\textwidth]{../img_icon/",changeToIconPNG(imageFiles),"}")
   
+  tx = data.frame(
+    N = 1:length(imageFilesX),
+    phase1 = imageFilesX,
+    descriptions1 = t(descriptions))
   
   imageFilesP3 = rep("",length(imageFiles))
   descriptionsP3 = rep("",length(descriptions))
   imageFilesXP3 = rep("",length(imageFilesX))
-  if(nrow(dxp3)>0 && ncol(dxp3)>0){
-    ix = paste0("image",1:16)
-    ix = ix[ix %in% names(dxp3)]
-    imageFilesP3= dxp3[ix]
-    ifx = unlist(imageFilesP3)
-    ifx = ifx[ifx %in% names(dxp3)]
-    descriptionsP3 = dxp3[ifx]
-    imageFilesXP3 = paste0("\\includegraphics[width=0.2\\textwidth]{img/",imageFilesP3,"}")
+  
+  if(nrow(dxp2)>0 && ncol(dxp2)>0){
+    imageFilesP2= dxp2[paste0("image",1:16)]
+    descriptionsP2 = dxp2[unlist(imageFilesP2)]
+    descriptionsP2 = t(descriptionsP2)
+    imageFilesXP2 = paste0("\\includegraphics[width=0.09\\textwidth]{../img_icon/",changeToIconPNG(imageFilesP2),"}")
+    tx$phase2 = imageFilesXP2
+    tx$descriptions2 = descriptionsP2
   }
   
-  x = paste("\\begin{longtable}{ll | ll}",
-            "  \\hline\nImg & Description & Img & Desription\n",
-  "\\endhead ",
-  "\\hline",
-  "\\endfoot",
-  "\\endlastfoot",
-  #\\begin{tabular}{ll}",
-      paste(imageFilesX,"&",descriptions,"&",
-            imageFilesXP3,"&",descriptionsP3,
-            "\\\\\\",collapse="\n"),
-  #"\\end{tabular}",
-  "\\end{longtable}",collapse="\n")
+  if(nrow(dxp3)>0 && ncol(dxp3)>0){
+    imageFilesP3= dxp3[paste0("image",1:16)]
+    descriptionsP3 = dxp3[unlist(imageFilesP3)]
+    descriptionsP3 = t(descriptionsP3)
+    imageFilesXP3 = paste0("\\includegraphics[width=0.09\\textwidth]{../img_icon/",changeToIconPNG(imageFilesP3),"}")
+    tx$phase3 = imageFilesXP3
+    tx$descriptions3 = descriptionsP3
+  }
   
-  return(x)
+  xt = xtable(tx)
+  align(xt) <- c("r","r",rep(c("l","p{5cm}"),length.out=ncol(tx)-1))
+  
+  return(print(xt,include.rownames=F,type="latex",
+               sanitize.text.function = function(x){x},
+               hline.after=c(-1, 0), tabular.environment = "longtable"))
+  
 }
 
-makeTex = FALSE
+
+# makeTable = function(dx,dxp3){
+#   ix = paste0("image",1:16)
+#   ix = ix[ix %in% names(dx)]
+#   imageFiles= dx[,ix]
+#   ifx = unlist(imageFiles)
+#   ifx = ifx[ifx %in% names(dx)]
+#   descriptions = dx[,ifx]
+#   imageFilesX = paste0("\\includegraphics[width=0.2\\textwidth]{img/",imageFiles,"}")
+#   
+#   
+#   imageFilesP3 = rep("",length(imageFiles))
+#   descriptionsP3 = rep("",length(descriptions))
+#   imageFilesXP3 = rep("",length(imageFilesX))
+#   if(nrow(dxp3)>0 && ncol(dxp3)>0){
+#     ix = paste0("image",1:16)
+#     ix = ix[ix %in% names(dxp3)]
+#     imageFilesP3= dxp3[ix]
+#     ifx = unlist(imageFilesP3)
+#     ifx = ifx[ifx %in% names(dxp3)]
+#     descriptionsP3 = dxp3[ifx]
+#     imageFilesXP3 = paste0("\\includegraphics[width=0.2\\textwidth]{img/",imageFilesP3,"}")
+#   }
+#   
+#   x = paste("\\begin{longtable}{ll | ll}",
+#             "  \\hline\nImg & Description & Img & Desription\n",
+#   "\\endhead ",
+#   "\\hline",
+#   "\\endfoot",
+#   "\\endlastfoot",
+#   #\\begin{tabular}{ll}",
+#       paste(imageFilesX,"&",descriptions,"&",
+#             imageFilesXP3,"&",descriptionsP3,
+#             "\\\\\\",collapse="\n"),
+#   #"\\end{tabular}",
+#   "\\end{longtable}",collapse="\n")
+#   
+#   return(x)
+# }
+
+makeTex = TRUE
 
 if(makeTex){
-  tex = "\\documentclass[12pt,a4paper]{article}
-  \\usepackage{graphicx}
-  \\usepackage{longtable}
-  \\usepackage[utf8]{inputenc}
-  \\usepackage{geometry}
-  \\begin{document}\n\n"
   for(i in 1:nrow(d)){
-    dxp3 = dp3[dp3$participantID == d$participantID[i],][1,]
-    try(tex <- paste(tex,"\n",makeLatex(d[i,],dxp3)))
+    tex = "\\documentclass[10pt,a4paper,landscape]{article}
+    \\usepackage{graphicx}
+    \\usepackage{longtable}
+    \\usepackage[utf8]{inputenc}
+    \\usepackage[margin=0.5in]{geometry}
+    \\begin{document}\n\n"
+    
+    dxp3 = data.frame()
+    dxp2 = data.frame()
+    if(!noP3Data){
+      dxp3 = dp3[dp3$participantID == d$participantID[i],][1,]
+      if(is.na(dxp3$participantID)){
+        dxp3 = dxp3[F,]
+      }
+    }
+    print(nrow(dxp3))
+    
+    if(!noP2Data){
+      dxp2 = dp2[match(dx$participantID,dp2$participantID),][1,]
+      if(is.na(dxp2$participantID)){
+        dxp2 = dxp2[F,]
+      }
+    }
+    
+     # dxp3 = dp3[dp3$participantID == d$participantID[i],][1,]
+    #  dxp2 = dp2[dp2$participantID == d$participantID[i],][1,]
+      tex <- paste(tex,"\n",makeLatex(d[i,],dxp2,dxp3))
+      tex = paste(tex,"\n","\\end{document}")
+      cat(tex,file = 
+            paste0("../results/StoryOrder/cleanData/tex/storyOrder_",d$participantID[i],".tex"))
   }
-  
-  tex = paste(tex,"\n","\\end{document}")
-  
-  cat(tex,file = "../results/StoryOrder/cleanData/storyOrder.tex")
+
 }
 #######
 
